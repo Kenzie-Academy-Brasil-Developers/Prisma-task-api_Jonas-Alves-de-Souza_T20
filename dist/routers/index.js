3c0d7ebdb1b3d20e045fd9354c3ffa3d1ff24d3a
Jonas Alves de Souza <souza.jonas.a@gmail.com>
2024-04-24 19:39:03 -0300

    remove dist

--- dist/routers/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserRouter = exports.CategoryRouter = exports.TaskRouter = void 0;
-var Tasks_router_1 = require("./Tasks.router");
-Object.defineProperty(exports, "TaskRouter", { enumerable: true, get: function () { return Tasks_router_1.TaskRouter; } });
-var Categories_router_1 = require("./Categories.router");
-Object.defineProperty(exports, "CategoryRouter", { enumerable: true, get: function () { return Categories_router_1.CategoryRouter; } });
-var use_router_1 = require("./use.router");
-Object.defineProperty(exports, "UserRouter", { enumerable: true, get: function () { return use_router_1.UserRouter; } });
--- dist/services/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserServices = exports.CategoryServices = exports.TasksServices = void 0;
-const Tasks_services_1 = require("./Tasks.services");
-Object.defineProperty(exports, "TasksServices", { enumerable: true, get: function () { return Tasks_services_1.TasksServices; } });
-const Categories_services_1 = require("./Categories.services");
-Object.defineProperty(exports, "CategoryServices", { enumerable: true, get: function () { return Categories_services_1.CategoryServices; } });
-const User_services_1 = require("./User.services");
-Object.defineProperty(exports, "UserServices", { enumerable: true, get: function () { return User_services_1.UserServices; } });
