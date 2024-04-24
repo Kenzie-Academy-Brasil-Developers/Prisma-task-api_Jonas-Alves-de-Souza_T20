338c2e08b31086d8a55d4229c06cc9815458c8c4
Jonas Alves de Souza <souza.jonas.a@gmail.com>
2024-04-24 00:36:39 -0300

    indenting code

--- src/middlewares/userAuthentication.middleware.ts
+++ src/middlewares/userAuthentication.middleware.ts
@@ -55,35 +55,33 @@ class userAuthentication{
                 res: Response, 
                 next: NextFunction
         
-            ) => {
-        
-                const taskId = req.params.id
-                const userId = res.locals.decode?.id
-        
+        ) => {
+            const taskId = req.params.id
+            const userId = res.locals.decode?.id
+
+            try {
                 
-                try {
-                    
-                    const task = await prisma.task.findFirst(
-                        { where: {id: Number(taskId)} }
-                    )
-                    if (!task) {
-                        throw new AppError(404, "Task not found");
-                    }
-        
-                    if (task?.userId !== userId) {
-                        throw new AppError(403, "This user is not the task owner")
-                    }
-        
-                    next()
-                } catch (error) {
-                    console.error('Error:', error)
-            
-                    if (error instanceof AppError) {
-                        return res.status(error.statusCode).json({ message: error.message });
-                    } 
-                        
-                    throw new AppError(500, "Internal Server Error")         
+                const task = await prisma.task.findFirst(
+                    { where: {id: Number(taskId)} }
+                )
+                if (!task) {
+                    throw new AppError(404, "Task not found");
                 }
+    
+                if (task?.userId !== userId) {
+                    throw new AppError(403, "This user is not the task owner")
+                }
+    
+                next()
+            } catch (error) {
+                console.error('Error:', error)
+        
+                if (error instanceof AppError) {
+                    return res.status(error.statusCode).json({ message: error.message });
+                } 
+                    
+                throw new AppError(500, "Internal Server Error")         
+            }
     }
 
     IsCategoryOwner = async (
